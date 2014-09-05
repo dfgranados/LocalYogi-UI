8ffa3ecc3b499400f686bc1ead2a81fdbdd12954
Dan Granados <dfgranadosreyes@gmail.com>
2014-09-05 13:26:47 -0400

    bottom nav bar now shows up only when logged in, and goes away when logged out

--- www/js/events.js
+++ www/js/events.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-        $( ".login-btn" ).click(function() {
-                          $('.bar-tab').css("display", "block");
-                });
+	/*show bottom navigation when a user has logged in*/
+	
+	function navBarVisibility() {
+		var hash = window.location.hash;
+		hash = hash.toString();
+		
+		if(hash === "")
+		{
+			$(".bar-tab").css("display", "none");
+		}
+		else
+		{
+			$(".bar-tab").css("display", "block");
+		}
+
+	}
+	setInterval(navBarVisibility, 1);
+
+	/*$( ".login-btn" ).on("click",function() {
+		$('.bar-tab').css("display", "block");
+	});*/
 
 });
